4e5797dbdee39100c9947341f0b46d017fa9b6ab
ranjeet16pd <91504447+ranjeet16pd@users.noreply.github.com>
2023-12-25 20:04:49 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div", {}, React.createElement("div", {},React.createElement("h1",{},"I am H1 inside the Div")));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
